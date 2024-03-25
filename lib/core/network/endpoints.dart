36184fbdb6125b17b463c4447bf18f5635edf15b
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-03-25 03:29:30 +0200

    Update Network and Validations
    Add Network layer and validations and Utils Files and Update Icon App

---
+++ lib/core/network/endpoints.dart
@@ -0,0 +1,68 @@
+String BASE_URL = 'https://student.valuxapps.com/api';
+
+class EndPoints {
+  // USER
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const LOGOUT = '/logout';
+  static const PROFILE = '/profile';
+  static const UPDATE_PROFILE = '/update-profile';
+  static const VERIFY_EMAIL = '/verify-email';
+  static const VERIFY_CODE = '/verify-code';
+  static const RESET_PASSWORD = '/reset-password';
+  static const CHANGE_PASSWORD = '/change-password';
+
+  // ADDRESSES
+  static const NEW_ADDRESS = '/addresses';
+  static const GET_ADDRESS = '/addresses';
+  static UpdateAddress(addressId) => '/addresses/$addressId';
+  static DeleteAddress(addressId) => '/addresses/$addressId';
+
+  //PRODUCTS
+  static CategoryProducts(categoryId) => '/products?category_id=$categoryId';
+  static ProductDetails(id) => '/products/$id';
+  static const SEARCH_PRODUCTS = '/products/search';
+
+  //FAVORITES
+  static const FAVORITES = '/favorites';
+  static const ADD_OR_DELETE_FAVOURITE = '/favorites';
+  static DeleteFavorite(productId) => '/favorites/$productId';
+
+  //CARTS
+  static const GET_CARTS = '/carts';
+  static const ADD_OR_RRMOVE_CART_WITH_PRODUCT_ID = '/carts';
+  static DeleteCart(id) => '/carts/$id';
+  static UpdateCart(id) => '/carts/$id';
+
+  //CONTACTS
+  static const GET_CONTACTS = '/contacts';
+
+  //CATEGORIES
+  static const GET_CATEGORIES = '/categories';
+  static CategoryDetails(categoryId) => '/categories/$categoryId';
+
+  //SETTINGS
+  static const GET_SETTINGS = '/settings';
+
+  //COMPLAINTS
+  static const ADD_COMPLAINTS = '/complaints';
+
+  //FAQS
+  static const GWT_FAQS = '/faqs';
+
+  //BANNERS
+  static const GET_BANNERS = '/banners';
+
+  //HOME
+  static const GET_HOME_DATA = '/home';
+
+  //ORDERS
+  static const ADD_ORDERS = '/orders';
+  static const GET_ORDERS = '/orders';
+  static OrderDetails(orderId) => '/orders/$orderId';
+  static const ESTIMATE_ORDER = '/estimate-order';
+  static CancelOrder(orderId) => '/orders/$orderId/cancel';
+
+  //NOTIFICATIONS
+  static const GET_NOTIFICATIONS = '/notifications';
+}
---
+++ lib/core/network/error_handling.dart
@@ -0,0 +1,40 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce/core/network/response/extension.dart';
+import 'package:ecommerce/core/network/response/failure.dart';
+import 'package:ecommerce/core/network/response/type.dart';
+
+class ErrorHandler implements Exception {
+  Failure? failure;
+
+  ErrorHandler.handle(dynamic error) {
+    if (error is DioException) {
+      failure = _handleError(error);
+    } else {
+      failure = NetworkResponse.DEFAULT.getMessage();
+    }
+  }
+}
+
+Failure _handleError(DioException error) {
+  switch (error.type) {
+    case DioExceptionType.connectionTimeout:
+      return NetworkResponse.CONNECT_TIMEOUT.getMessage();
+    case DioExceptionType.sendTimeout:
+      return NetworkResponse.SEND_TIMEOUT.getMessage();
+    case DioExceptionType.receiveTimeout:
+      return NetworkResponse.RECIEVE_TIMEOUT.getMessage();
+    case DioExceptionType.badResponse:
+      if (error.response != null &&
+          error.response?.statusCode != null &&
+          error.response?.statusMessage != null) {
+        return Failure(error.response?.statusCode ?? 0,
+            error.response?.data["message"] ?? "");
+      } else {
+        return NetworkResponse.DEFAULT.getMessage();
+      }
+    case DioExceptionType.cancel:
+      return NetworkResponse.CANCEL.getMessage();
+    default:
+      return NetworkResponse.DEFAULT.getMessage();
+  }
+}
