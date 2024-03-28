cdddb5c2b746fb96fe3fa9b26d2993833a689e72
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-03-28 02:10:07 +0200

    add app colors
    add app colors to utils folder

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,1 +1,17 @@
-class AppColors {}
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blue_ocean = Color(0xff3669C9);
+  static const orange_fresh = Color(0xffFDA429);
+  static const navy_black = Color(0xff13181F);
+  static const white = Color(0xffFFFFFF);
+  static const earth_green = Color(0xff3A9B7A);
+  static const red_velvet = Color(0xffC93545);
+  static const dark_grey = Color(0xffFAFAFA);
+  static const half_grey = Color(0xff838589);
+  static const soft_grey = Color(0xffEDEDED);
+  static const off_grey = Color(0xffFAFAFA);
+  static const off_blue = Color(0xffEFF5FB);
+  static const off_green = Color(0xffEEFAF6);
+  static const off_red = Color(0xffFCECEF);
+}
