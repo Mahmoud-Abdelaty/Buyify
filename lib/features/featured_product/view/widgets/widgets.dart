79c4282ea203c0db51eae9a42a54bc617021cbc2
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-05-01 02:42:30 +0300

    Featured Product Finished

---
+++ lib/features/featured_product/view/widgets/widgets.dart
@@ -0,0 +1,7 @@
+import 'package:Buyify/core/utils/app_colors.dart';
+import 'package:Buyify/features/home/data/models/home_model.dart';
+import 'package:Buyify/features/home/view/widget/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+part '../featured_product.dart';
