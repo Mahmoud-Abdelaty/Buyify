02ad221e600656c0f3e446077cf5c53fd2ed5058
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-05-12 19:06:08 +0300

    Update on User Profile

---
+++ lib/features/user_profile/data/repo/user_profile_repo.dart
@@ -0,0 +1,7 @@
+import 'package:Buyify/core/network/response/failure.dart';
+import 'package:Buyify/features/user_profile/data/models/user_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UserProfileRepo {
+  Future<Either<Failure?, UserModel>> getUserData();
+}
