54b9324dcee9dc01683f076704161eb75d2ed624
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-05-20 01:53:02 +0300

    Add Home Loading View

--- lib/features/home/view/widget/home_loading.dart
+++ lib/features/home/view/widget/home_loading.dart
@@ -5,18 +5,42 @@ class HomeLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Shimmer.fromColors(
-      baseColor: AppColors.grey,
-      highlightColor: Colors.white,
+    return SingleChildScrollView(
       child: Column(
         children: [
           SearchBox(),
           Container(
-            height: 115.h,
-            width: 315.w,
+            height: 150.h,
+            width: 305.w,
+            alignment: Alignment.centerLeft,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10.r),
-              color: Colors.cyan,
+              color: AppColors.alabaster,
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(left: 30.w, top: 55.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 150.w,
+                    height: 20.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.r),
+                      color: AppColors.grey93,
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                  Container(
+                    width: 92.w,
+                    height: 10.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.r),
+                      color: AppColors.grey93,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Padding(
@@ -31,6 +55,7 @@ class HomeLoading extends StatelessWidget {
                   child: ListView.separated(
                     padding: EdgeInsets.symmetric(horizontal: 20.w),
                     scrollDirection: Axis.horizontal,
+                    physics: NeverScrollableScrollPhysics(),
                     itemBuilder: (context, index) => SizedBox(
                       child: Column(
                         children: [
@@ -38,11 +63,19 @@ class HomeLoading extends StatelessWidget {
                             width: 70.w,
                             height: 60.h,
                             decoration: BoxDecoration(
-                              color: Colors.cyan,
+                              color: AppColors.grey93,
                               borderRadius: BorderRadius.circular(10.r),
                             ),
                           ),
                           SizedBox(height: 8.h),
+                          Container(
+                            height: 10.h,
+                            width: 60.w,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5.r),
+                              color: AppColors.grey93,
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -50,59 +83,108 @@ class HomeLoading extends StatelessWidget {
                     itemCount: 4,
                   ),
                 ),
+                TitleSections(
+                  title: 'Featured Product',
+                ),
+                SizedBox(
+                  height: 245.h,
+                  child: ListView.separated(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => Container(
+                      width: 156.w,
+                      decoration: BoxDecoration(
+                        color: AppColors.alabaster,
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsets.fromLTRB(10.w, 15.h, 16.w, 20.h),
+                            child: Center(
+                              child: Container(
+                                width: 136.w,
+                                height: 125.h,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5.r),
+                                  color: AppColors.grey93,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(left: 10.w),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 100.w,
+                                  height: 20.h,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5.r),
+                                    color: AppColors.grey93,
+                                  ),
+                                ),
+                                SizedBox(height: 10.h),
+                                Container(
+                                  width: 100.w,
+                                  height: 20.h,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5.r),
+                                    color: AppColors.grey93,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    separatorBuilder: (context, index) => SizedBox(width: 20.w),
+                    itemCount: 4,
+                  ),
+                ),
               ],
             ),
           ),
-          SizedBox(height: 5.h),
-          // Container(
-          //   decoration: BoxDecoration(
-          //     color: AppColors.alabaster,
-          //     borderRadius: BorderRadius.only(
-          //       topLeft: Radius.circular(10.r),
-          //       topRight: Radius.circular(10.r),
-          //     ),
-          //   ),
-          //   child: Column(
-          //     children: [
-          //       ProductSection(productList: state.data.products),
-          //       Padding(
-          //         padding: EdgeInsets.symmetric(vertical: 36.h),
-          //         child: ImageBox(imageUrl: state.data.ad),
-          //       ),
-          //     ],
-          //   ),
-          // ),
+          Container(
+            height: 150.h,
+            width: 327.w,
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.r),
+              color: AppColors.alabaster,
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(left: 30.w, top: 55.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 150.w,
+                    height: 20.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.r),
+                      color: AppColors.grey93,
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                  Container(
+                    width: 92.w,
+                    height: 10.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.r),
+                      color: AppColors.grey93,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-// return SingleChildScrollView(
-//   child: Column(
-//     children: [
-//       SearchBox(),
-//       Banners(list: state.data.banners),
-//       const CategorySection(),
-//       SizedBox(height: 5.h),
-//       Container(
-//         decoration: BoxDecoration(
-//           color: AppColors.alabaster,
-//           borderRadius: BorderRadius.only(
-//             topLeft: Radius.circular(10.r),
-//             topRight: Radius.circular(10.r),
-//           ),
-//         ),
-//         child: Column(
-//           children: [
-//             ProductSection(productList: state.data.products),
-//             Padding(
-//               padding: EdgeInsets.symmetric(vertical: 36.h),
-//               child: ImageBox(imageUrl: state.data.ad),
-//             ),
-//           ],
-//         ),
-//       ),
-//     ],
-//   ),
-// );
