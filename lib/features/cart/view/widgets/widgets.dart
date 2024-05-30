76afb8656a99335e396fe91ebdad27597057afc6
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-05-30 22:07:29 +0300

    Some Updates

--- lib/features/cart/view/widgets/widgets.dart
+++ lib/features/cart/view/widgets/widgets.dart
@@ -9,6 +9,7 @@ import 'package:Buyify/features/cart/data/models/cart_model.dart';
 import 'package:Buyify/features/cart/data/repo/cart_repo_impl.dart';
 import 'package:Buyify/features/home/view/widget/widgets.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
